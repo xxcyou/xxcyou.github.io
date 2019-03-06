b830517586cd4adf8fb9a847df0d7eb90c36f4fb
xxcyou <xxcyou@xxcyou.com>
2019-03-07 02:20:20 +0800

    刚刚

--- xxcyou.js
+++ xxcyou.js
@@ -5,19 +5,19 @@ function Sfou(patho) {
 }
 window.onload = function() {/*加载完毕*/
 	var path = location.href;
-	if (path.indexOf("m.iqiyi.com") != -1 && path.indexOf(".html") != -1 && path.indexOf("api.ledboke.com") == -1)//爱奇艺
+	if (path.indexOf("m.iqiyi.com") != -1 && path.indexOf(".html") != -1 && path.indexOf("api.ledboke.com") == -1)/*爱奇艺*/
 	{
 		Sfou(path);
 	}
-	if (path.indexOf("m.le.com") != -1 && path.indexOf(".html") != -1 && path.indexOf("api.ledboke.com") == -1)//乐视
+	if (path.indexOf("m.le.com") != -1 && path.indexOf(".html") != -1 && path.indexOf("api.ledboke.com") == -1)/*乐视*/
 	{
 		Sfou(path);
 	}
-	if (path.indexOf("m.v.qq.com") != -1 && path.indexOf("play.html") != -1 && path.indexOf("api.ledboke.com") == -1)//腾讯视频
+	if (path.indexOf("m.v.qq.com") != -1 && path.indexOf("play.html") != -1 && path.indexOf("api.ledboke.com") == -1)/*腾讯视频*/
 	{
 		Sfou(path);
 	}
-	if (path.indexOf("m.youku.com") != -1 && path.indexOf(".html") != -1 && path.indexOf("api.ledboke.com") == -1)//优酷
+	if (path.indexOf("m.youku.com") != -1 && path.indexOf(".html") != -1 && path.indexOf("api.ledboke.com") == -1)/*优酷*/
 	{
 		Sfou(path);
 	}
